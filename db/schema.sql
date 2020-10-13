6164568b5683b52da82fc0fcc829ca1d2df15b2e
John Alonso <jvalon9455@gmail.com>
2020-10-13 13:36:35 -0400

    created tables and seeds in db folders

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,46 @@ DROP DATABASE IF EXISTS project2;
 -- Creates the "project2" database --
 CREATE DATABASE project2;
 
+USE project2;
+
+CREATE TABLE trainer
+(
+	id int NOT NULL AUTO_INCREMENT,
+	trainer_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE athlete
+(
+	id int NOT NULL AUTO_INCREMENT,
+	athlete_name varchar(255) NOT NULL,
+	sport varchar(255) NOT NULL,
+    injury_site varchar(255) NOT NULL,
+    injury varchar(255) NOT NULL
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE treatment
+(
+	id int NOT NULL AUTO_INCREMENT,
+	treatment_name varchar(255) NOT NULL,
+    length_of_time INTEGER NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE rehab
+(
+	id int NOT NULL AUTO_INCREMENT,
+	exercise varchar(255) NOT NULL,
+	num_sets INTEGER NOT NULL NOT NULL,
+    num_reps INTEGER NOT NULL NOT NULL,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM trainer;
+
+SELECT * FROM athlete;
+
+SELECT * FROM treatment;
+
+SELECT * FROM rehab;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO trainer (trainer_name) VALUES ('Jimmy Alonso');
+
+INSERT INTO athlete (athlete_name, sport, injury_site, injury) 
+VALUES ('Jeff Flynn', 'Football', 'Shoulder', 'AC Sprain');
+
+INSERT INTO treatment (treatment_name, length_of_time) 
+VALUES ('E-stim', 15);
+
+INSERT INTO rehab (exercise, num_sets, num_reps) 
+VALUES ('ATYs', 3, 12);
