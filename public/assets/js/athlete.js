16f139e4ec30ce1f30beb6b7016645ca4b51ede9
Insomnia <jamie.mar.jones14@gmail.com>
2020-10-15 12:52:25 -0400

    added a form

--- public/assets/js/athlete.js
+++ public/assets/js/athlete.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
-    const nameInput = $("#athlete-name");
-    const athleteList = $("tbody");
-    const athleteContainer = $(".author-container");
+$(document).ready(function () {
+  const nameInput = $("#athlete-name");
+  const athleteList = $("tbody");
+  const athleteContainer = $(".author-container");
 
+  $("#add-athlete").click(function () {
+    $("add-athlete-form").show();
+  });
+});
