5f7d8164dad4b5e2bf97e9e648a337aa26a811de
John Alonso <jvalon9455@gmail.com>
2020-10-18 17:50:33 -0400

    cleaned up code for cleaner readability

--- controllers/trainersController.js
+++ controllers/trainersController.js
@@ -6,124 +6,38 @@ const db = require("../models");
 //VIEWS ROUTES
 
 router.get("/trainer", (req, res) => {
-    db.Trainer.findAll().then((allTrainers) => {
-        res.render("trainers/trainers", { allTrainers: allTrainers });
-    });
+  db.Trainer.findAll().then((allTrainers) => {
+    res.render("trainers/trainers", { allTrainers: allTrainers });
+  });
 });
 
 
-// router.get ("/trainers/new", (req, res) => {
-//     res.render("trainers/new-trainer");
-// });
-
-// router.get ("/trainer/:id", (req, res) => {
-//     db.Trainer.findOne({
-//         where: {
-//             id: res.params.id,
-//         },
-//     }).then((foundTrainer) => {
-//         console.log(foundTrainer);
-//         res.render("single-trainer", {
-//             trainer_name: foundTrainer.trainer_name,
-//             id: foundTrainer.id,
-//         });
-//     });
-// });
-//     res.render("trainers/single-trainer");
-// });
-
-// router.get("/thing/:id", (req, res) => {
-//     db.Thing.findOne({
-//       where: {
-//         id: req.params.id,
-//       },
-//     }).then((foundThing) => {
-//       console.log(foundThing);
-//       res.render("single-thing", {
-//         name: foundThing.name,
-//         price: foundThing.price,
-//         id: foundThing.id,
-//       });
-//     });
-//   });
-
-router.get ("/trainers/:id/edit", (req, res) => {
-    res.render("trainers/edit-trainer");
+router.get("/trainers/:id/edit", (req, res) => {
+  res.render("trainers/edit-trainer");
 });
-
-
 
 
 // //API ROUTES
 
 router.post("/api/trainer", (req, res) => {
-    console.log(req.body);
-    db.Trainer.create(req.body)
-      .then((newTrainer) => {
-        res.json({
-          error: false,
-          data: newTrainer,
-          message: "Successfully created new trainer.",
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: true,
-          data: null,
-          message: "Unable to create new trainer.",
-        });
+  console.log(req.body);
+  db.Trainer.create(req.body)
+    .then((newTrainer) => {
+      res.json({
+        error: false,
+        data: newTrainer,
+        message: "Successfully created new trainer.",
       });
-  });
-
-//   router.put("/api/trainer/:id", (req, res) => {
-//       db.Thing.update(req.body, {
-//           where: {
-//               id: req.params.id,
-//           },
-//       }).then((updatedTrainer) => {
-//           console.log(updatedTrainer);
-//           res.end();
-//       });
-//   });
-
-
-//   router.put("/api/thing/:id", (req, res) => {
-//     db.Thing.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     }).then((updatedObject) => {
-//       console.log(updatedObject);
-//       res.end();
-//     });
-//   });
-
-
-// router.post("/api/trainers", function(req, res) {
-//     trainer.create([
-//       "trainer_name" 
-//     ], [
-//       req.body.trainer_name,
-//     ], function(result) {
-//       // Send back the ID of the new quote
-//       res.json({ id: result.insertId });
-//     });
-//   });
-
-
-
-
-
-
-//API ROUTES
-
-
-
-
-
-
-
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create new trainer.",
+      });
+    });
+});
 
 
 module.exports = router;
--- public/assets/js/treatment.js
+++ public/assets/js/treatment.js
@@ -4,21 +4,15 @@ $(document).ready(function () {
   const treatment_name = $("#treatment_name");
   const progress_note = $("#progress_note");
   const athlete = $("#athlete");
-  
-  //   $("#add-athlete").click(function () {
-  //     $("add-athlete-form").toggle();
-  //     $("add-athlete").hide();
-  //     console.log("clicked");
-  //   });
+
 
   $("#submit-treatment").click(function (event) {
     event.preventDefault();
-    // upsertAthlete();
     console.log("submit");
     if (
       !treatment_name.val().trim() ||
-      !progress_note.val().trim() 
-     ) {
+      !progress_note.val().trim()
+    ) {
       return;
     }
     athleteTreatment({
@@ -28,20 +22,6 @@ $(document).ready(function () {
     });
   });
 
-  // function handleAthleteFormSubmit(event) {
-  //   event.preventDefault();
-  //   if (
-  //     !nameInput.val().trim() ||
-  //     !injurySiteInput.val().trim() ||
-  //     !injuryInput.val().trim()
-  //   ) {
-  //     return;
-  //   }
-  //   athleteTreatment({
-  //     treatment_name: nameOfTreatment.val().trim(),
-  //     length_of_time: lengthOfTime.val().trim(),
-  //   });
-  // }
 
   function athleteTreatment(treatmentAthlete) {
     $.post("/api/treatment", treatmentAthlete).then(() => location.reload());
